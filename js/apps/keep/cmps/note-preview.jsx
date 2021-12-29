73ae75ed4507b4c2bf5b73cc955eb4d7658a3343
LiorAki <lior21231@gmail.com>
2021-12-29 22:25:27 +0200

    test

--- js/apps/keep/cmps/note-preview.jsx
+++ js/apps/keep/cmps/note-preview.jsx
@@ -1,14 +1,35 @@
 import { noteService } from "../services/note.service.js";
 const { Link } = ReactRouterDOM
 
-export function NotePreview({note}){
-    return (
-        <Link className="clean-link" to={`/note/${note.id}`}>
-            <article className="note-preview">
-                <div>XXXX</div>
-            </article>
-        </Link>
-    )
+// export function NotePreview({note}){
+//     return (
+//         <Link className="clean-link" to={`/note/${note.id}`}>
+//             <article className="note-preview">
+//                 <div></div>
+//             </article>
+//         </Link>
+//     )
     
-}
+// }
 
+export class NotePreview extends React.Component {
+    state ={
+        note:null,
+        noteStyle:{}
+    }
+
+    componentDidMount(){
+        const note = this.props.note;
+        this.setState({note})
+    }
+    render() {
+        const { note } = this.state;
+        if(!note) return <h2>no notes</h2>
+        return (
+            <article className="note-preview">
+                <NoteEdit
+                note = {note} />
+            </article>
+        )
+    }
+}
