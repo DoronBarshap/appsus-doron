1bff4bb42afa5259218e3b0c77fda0feb5b1ea42
LiorAki <lior21231@gmail.com>
2021-12-29 11:01:37 +0200

    added services folder, added changes to HTML page

--- App.jsx
+++ App.jsx
@@ -1,1 +1,23 @@
-console.log('AppSus');
+
+import { AppHome } from './js/pages/app-home.jsx';
+import { AppHeader } from './cmps/AppHeader.jsx';
+import { AppFooter } from './cmps/AppFooter.jsx';
+
+const Router = ReactRouterDOM.HashRouter;
+const { Route, Switch } = ReactRouterDOM;
+
+export function App(){
+    return(
+    <Router>
+        <header>
+            <AppHeader/>
+        </header>
+        <main className="app-main">
+            <Route component={AppHome} path="/"/>
+        </main>
+        <footer>
+            <AppFooter/>
+        </footer>
+    </Router>
+    )
+}
